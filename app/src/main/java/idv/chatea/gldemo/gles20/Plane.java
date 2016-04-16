fb4a2d19bef10708913c0220c0a0bbe6dc519dcc
chatea <cha122977@gmail.com>
2016-04-16 11:58:20 +0800

    Add plane

---
+++ app/src/main/java/idv/chatea/gldemo/gles20/Plane.java
@@ -0,0 +1,98 @@
+package idv.chatea.gldemo.gles20;
+
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import idv.chatea.gldemo.gles20.Utils;
+
+public class Plane {
+    private static final String VERTEX_SHADER_CODE =
+            "uniform mat4 uMVPMatrix;" +
+            "attribute vec3 aPosition;" +
+            "void main() {" +
+            "  gl_Position = uMVPMatrix * vec4(aPosition, 1.0);" +
+            "}";
+
+    private static final String FRAGMENT_SHADER_CODE =
+            "precision mediump float;" +
+            "uniform vec4 uColor;" +
+            "void main() {" +
+            "  gl_FragColor = uColor;" +
+            "}";
+
+    private static final float[] VERTEX_DATA = {
+        -0.5f, -0.5f, 0f,
+         0.5f, -0.5f, 0f,
+        -0.5f,  0.5f, 0f,
+         0.5f,  0.5f, 0f,
+    };
+
+    private static final byte[] INDEX_DATA = {
+            0, 1, 2, 2, 1, 3,
+    };
+
+    private static final int BYTES_PER_FLOAT = Float.SIZE / Byte.SIZE;
+
+    private static final float[] COLOR = {1.0f, 1.0f, 1.0f, 1.0f};
+
+    private int[] mVertexGLBuffer = new int[1];
+    private int[] mIndexGLBuffer = new int[1];
+
+    private float[][] portalModuleMatrices = new float[2][];
+
+    private int mProgram;
+    private int mMVPMatrixHandle;
+    private int mPositionHandle;
+    private int mColorHandle;
+
+    public Plane() {
+        ByteBuffer vbb = ByteBuffer.allocateDirect(VERTEX_DATA.length * BYTES_PER_FLOAT);
+        vbb.order(ByteOrder.nativeOrder());
+        vbb.asFloatBuffer().put(VERTEX_DATA);
+        vbb.position(0);
+
+        GLES20.glGenBuffers(1, mVertexGLBuffer, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mVertexGLBuffer[0]);
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, vbb.capacity(), vbb, GLES20.GL_STATIC_DRAW);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+
+        ByteBuffer ibb = ByteBuffer.allocateDirect(INDEX_DATA.length);
+        ibb.order(ByteOrder.nativeOrder());
+        ibb.put(INDEX_DATA);
+        ibb.position(0);
+
+        GLES20.glGenBuffers(1, mIndexGLBuffer, 0);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, mIndexGLBuffer[0]);
+        GLES20.glBufferData(GLES20.GL_ELEMENT_ARRAY_BUFFER, ibb.capacity(), ibb, GLES20.GL_STATIC_DRAW);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        mProgram = GLES20.glCreateProgram();
+        mProgram = Utils.createProgram(VERTEX_SHADER_CODE, FRAGMENT_SHADER_CODE);
+
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "uColor");
+    }
+
+    public void draw(float[] mvpMatrix) {
+        GLES20.glUseProgram(mProgram);
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mVertexGLBuffer[0]);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, mIndexGLBuffer[0]);
+
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+        GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false, 0, 0);
+        GLES20.glUniform4fv(mColorHandle, 1, COLOR, 0);
+
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, INDEX_DATA.length, GLES20.GL_UNSIGNED_BYTE, 0);
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+        GLES20.glUseProgram(0);
+    }
+}
